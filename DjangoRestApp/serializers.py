308047181f5cfe4e8e208f3e6694f79ec575a26c
Yogesh <yogirakate>
2020-08-26 21:06:21 +0530

    first commit

---
+++ DjangoRestApp/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from DjangoRestApp.models import Posts
+
+class PostSerializers(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = Posts
+		fields = ['post_title', 'post_description']
